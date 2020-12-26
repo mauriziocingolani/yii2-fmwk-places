79df076a731a84971954604ed902b09b59013962
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2020-12-26 15:15:12 +0100

    Classe NazioneIT

---
+++ NazioneIt.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace mauriziocingolani\places;
+
+use mauriziocingolani\yii2fmwkphp\ActiveRecord;
+
+/**
+ * @property integer $NazioneID
+ * @property string $Nazione
+ */
+class NazioneIT extends ActiveRecord {
+
+    const ITALIA = 106;
+
+    public static function tableName(): string {
+        return 'places_nazioni';
+    }
+
+    /* Validators */
+    /* Relazioni */
+    /* Eventi */
+    /* Metodi */
+    /* Getters-Setters */
+    /* Metodi statici */
+
+    /**
+     * @return NazioneIT[]
+     */
+    public static function GetAll() {
+        return self::find()->orderBy('Nazione')->all();
+    }
+
+}
